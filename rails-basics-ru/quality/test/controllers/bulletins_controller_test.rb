49b87151f3df943c7c8f52d42c54f9118f464839
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2022-12-18 16:13:44 +0300

    Refactoring

--- rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
+++ rails-basics-ru/quality/test/controllers/bulletins_controller_test.rb
@@ -1,38 +1,21 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class BulletinsControllerTest < ActionDispatch::IntegrationTest
-  test 'opens bulletins page' do
-    get bulletins_url
-    assert_response :success
-    assert_select 'h1', 'Bulletins'
+  setup do
+    @bulletin = bulletins :bulletin1
   end
 
-  test 'bulletins page has links to bulletins' do
-    # TODO: написать функцию, которая будет по фикстурам создавать записи в БД
-    Bulletin.create title: 'Title 1', body: 'Body 1'
-    Bulletin.create title: 'Title 2', body: 'Body 2'
-
+  test 'should get index' do
     get bulletins_url
 
-    assert_select 'a', 'Title 1'
-    assert_select 'a', 'Title 2'
-  end
-
-  test 'opens one bulletin page' do
-    bulletin = bulletins :bulletin_1
-  
-    get bulletin_url bulletin
-  
     assert_response :success
-    assert_select 'h1', 'Bulletin page'
   end
 
-  test 'one bulletin page has correct title and body' do
-    bulletin = bulletins :bulletin_1
+  test 'should get bulletin' do
+    get bulletin_url @bulletin
 
-    get bulletin_url bulletin
-
-    assert_select 'p', 'Title 1'
-    assert_select 'p', 'Body 1'
+    assert_response :success
   end
 end
