2dcf7307117bd60576b6d038e9a241756aa9b343
Dmitry Kurtsev <dmitrykurtsev34@gmail.com>
2023-01-04 14:05:22 +0300

    Refactoring

--- rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
+++ rails-basics-ru/middlewares/app/middlewares/set_locale_middleware.rb
@@ -14,7 +14,11 @@ class SetLocaleMiddleware
     request = Rack::Request.new env
     locale = extract_locale request
 
-    I18n.locale = locale if locale
+    if locale
+      formated_locale = locale.downcase.to_sym
+
+      I18n.locale = I18n.available_locales.include?(formated_locale) ? formated_locale : I18n.default_locale
+    end
 
     @app.call env
   end
